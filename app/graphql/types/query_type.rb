ca9ffbbcae27b3863584536964a5777b7c059fd7
tegnike <user@mbp.local>
2020-11-06 16:46:28 +0100

    リンクモデルとユーザモデルをVoteモデルに関連付け

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,20 @@ module Types
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
     field :all_links, [LinkType], null: false
+    field :all_users, [UserType], null: false
+    field :all_votes, [VoteType], null: false
 
     # this method is invoked, when `all_link` fields is being resolved
     def all_links
       Link.all
+    end
+
+    def all_users
+      User.all
+    end
+
+    def all_votes
+      Vote.all
     end
   end
 end
